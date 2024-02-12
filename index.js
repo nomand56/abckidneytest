74684b27c2424496dc82b8087d3276f7b470060c
notnoman <noman.ahmad@thetechtower.com>
2024-02-12 01:00:45 -0800

    updated code

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ app.use("/api/v1", require("./routes/openAccess"));
 
 
 // Serve the React app on all other routes
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 
 const PORT = process.env.PORT || 3001
